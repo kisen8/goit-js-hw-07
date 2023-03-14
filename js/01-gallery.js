814e69298e870fe05f61f499206e0c8de5649ea4
Ihor Komar <kisenbenz8@gmail.com>
2023-03-14 14:43:48 +0200

    ReWork)

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -11,9 +11,56 @@ import { galleryItems } from "./gallery-items.js";
 
 // 5.Замена значения атрибута src элемента < img > в модальном окне перед открытием.Используй готовую разметку модального окна с изображением из примеров библиотеки basicLightbox.
 
-console.log(galleryItems);
+// console.log(galleryItems);
 
-// <div class="gallery__item">
+// -----------------------------------------------------------
+// const gallery = document.querySelector(".gallery");
+// const galleryMarkup = createGalleryItems(galleryItems);
+// gallery.insertAdjacentHTML("beforeend", galleryMarkup);
+
+// // console.log(createGalleryItems(galleryItems));
+
+// gallery.addEventListener("click", onGalleryClick);
+
+// function createGalleryItems(galleryItems) {
+//   return galleryItems
+//     .map(({ preview, original, description }) => {
+//       return `
+//          <div class="gallery__item">
+//              <a class="gallery__link" href="${original}">
+//                  <img
+//                     class="gallery__image"
+//                     src="${preview}"
+//                     data-source="${original}"
+//                     alt="${description}"
+//                 />
+//             </a>
+//         </div>`;
+//     })
+//     .join("");
+// }
+
+// function onGalleryClick(evt) {
+//   evt.preventDefault();
+//   if (evt.target.nodeName !== "IMG") {
+//     return;
+//   }
+
+//   const selectedImage = evt.target.getAttribute("data-source");
+//   console.log(evt.target);
+//   const instance = basicLightbox.create(`
+//     <img src="${selectedImage}" width="800" height="600">
+// `);
+
+//   instance.show();
+//   gallery.addEventListener("keydown", (e) => {
+//     if (e.key === "Escape") {
+//       instance.close();
+//     }
+//   });
+// }
+// ----------------------------------------------
+// {/* <div class="gallery__item">
 //   <a class="gallery__link" href="large-image.jpg">
 //     <img
 //       class="gallery__image"
@@ -22,47 +69,43 @@ console.log(galleryItems);
 //       alt="Image description"
 //     />
 //   </a>
-// </div>;
-const gallery = document.querySelector(".gallery");
-const galleryMarkup = createGalleryItems(galleryItems);
-gallery.insertAdjacentHTML("beforeend", galleryMarkup);
+// </div>; */}
+const galleryList = document.querySelector(".gallery");
+const galleryMarkup = createGalleryList(galleryItems);
+galleryList.insertAdjacentHTML("beforeend", galleryMarkup);
+galleryList.addEventListener("click", onClick);
 
-// console.log(createGalleryItems(galleryItems));
-
-gallery.addEventListener("click", onGalleryClick);
-
-function createGalleryItems(galleryItems) {
+function createGalleryList(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
-      return `
-         <div class="gallery__item">
-             <a class="gallery__link" href="${original}">
-                 <img
-                    class="gallery__image"
-                    src="${preview}"
-                    data-source="${original}"
-                    alt="${description}"
-                />
-            </a>
-        </div>`;
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
     })
     .join("");
 }
 
-function onGalleryClick(evt) {
-  evt.preventDefault();
-  if (evt.target.nodeName !== "IMG") {
+function onClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
     return;
   }
+  const selectImage = e.target.dataset.source;
+  // console.log(selectImage);
 
-  const selectedImage = evt.target.getAttribute("data-source");
-  console.log(evt.target);
   const instance = basicLightbox.create(`
-    <img src="${selectedImage}" width="800" height="600">
+    <img src="${selectImage}" width="800" height="600">
 `);
 
   instance.show();
-  gallery.addEventListener("keydown", (e) => {
+  galleryList.addEventListener("keydown", (e) => {
     if (e.key === "Escape") {
       instance.close();
     }
